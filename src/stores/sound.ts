b0c6c5533ef5c4bf27629eaa41661de11e1c3e54
parallel <parallelkim12@gmail.com>
2022-12-30 16:55:58 +0900

    audio 객체가 structed clone 되지 않음

--- src/stores/sound.ts
+++ src/stores/sound.ts
@@ -8,18 +8,15 @@ type STORETYPE = {
   removeAll: () => void;
 };
 
-const typeSound = new Audio("sounds/type.mp3");
-const enterSound = new Audio("sounds/line_break.mp3");
-
 const soundStore = create<STORETYPE>((set) => ({
   sound: [],
   playTypeSound: () =>
     set((state) => ({
-      sound: [...state.sound, structuredClone(typeSound)],
+      sound: [...state.sound, new Audio("sounds/type.mp3")],
     })),
   playEnterSound: () =>
     set((state) => ({
-      sound: [...state.sound, structuredClone(enterSound)],
+      sound: [...state.sound, new Audio("sounds/line_break.mp3")],
     })),
   removeSound: () => set((state) => ({ sound: state.sound.slice(1, state.sound.length) })),
   removeAll: () => set({ sound: [] }),

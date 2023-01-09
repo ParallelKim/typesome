a30ef5adde52031fb5aa431d2ef30e6184bd2bec
parallel <parallelkim12@gmail.com>
2023-01-09 12:14:31 +0900

    출력되는 문자에 폰트 추가

--- src/components/models/DefaultText.tsx
+++ src/components/models/DefaultText.tsx
@@ -8,8 +8,9 @@ type PROP = {
 };
 
 const DefaultText = ({ position = [0, 0, 0], scale = 1, size, value }: PROP) => {
+  const font = new FontFace("typewritic", "url(./typewritic.woff)");
   return (
-    <Text position={position} scale={[scale, scale, scale]} color={"black"} anchorX="center" anchorY="bottom" fontSize={size}>
+    <Text position={position} scale={[scale, scale, scale]} color={"black"} anchorX="center" anchorY="bottom" fontSize={size} font={"fonts/main.ttf"}>
       {value}
     </Text>
   );
